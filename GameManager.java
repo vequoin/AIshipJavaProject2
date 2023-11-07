df745d3df58596dd0783b950f572d307186724b7
BeanSprout53 <146155435+BeanSprout53@users.noreply.github.com>
2023-11-06 20:27:26 -0500

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -1,7 +1,5 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+
 
 public class GameManager {
     private Ship ship;
@@ -16,80 +14,324 @@ public class GameManager {
     private double[][] knowledgeGrid;
     private Random random = new Random();
     private List<Cell> Probability_list;
-    private int numOpenCells = ship.getOpenCells().size();
+    private int numOpenCells;
+    private double alpha; // New variable for alpha
+    private double[][] probabilityGrid;
+    
+    public static void main(String[] args) {
+        int shipSize = 50; // Set your ship size here
+        int botStrategy = 1; // Set your bot strategy here
+        int k = 5; // Set your 'k' value here
+        double alpha = 1; // Set your alpha value here
 
-    public GameManager(int shipSize, int botStrategy, int k) {
+        GameManager gameManager = new GameManager(shipSize, botStrategy, k, alpha);
+    }
+   
+    public GameManager(int shipSize, int botStrategy, int k, double alpha) {
         this.ship = new Ship(shipSize);
         this.shipSize = shipSize;
         this.k = k;
         this.botPosition = getRandomOpenCell(ship.getOpenCells());
         this.leakPosition = getRandomOpenCell(ship.getOpenCells());
-        
         this.coveredGrid = new ArrayList<>();
         this.botStrategy = botStrategy;
-        this.bot = new Bot(ship, botStrategy, botPosition, k);
+        //this.bot = new Bot(ship, botStrategy, botPosition, k);
         this.visitedNodes = new HashSet<>();
-        this.knowledgeGrid = new String[shipSize][shipSize];
-        initializeKnowledgeGrid();
-        initializeLeaks();
+        this.knowledgeGrid = new double[shipSize][shipSize]; // Changed from String to double
+        
+        this.alpha = alpha; // Initialize alpha
+        this.probabilityGrid = new double[shipSize][shipSize];
+        initializeProbabilityGrid(); // Initialize probability grid
+        this.numOpenCells = ship.getOpenCells().size();
+        int totalActions = 0;
+        totalActions = Game();
+        System.out.println(totalActions);
+        
+
+        
+        
+
+    }
+
+    private int Game(){
+        int actions = 0;
+        boolean beep = false;
+        while(botPosition!=leakPosition){
+            updateBotProbabilities();
+            beep = sense();
+            actions+=1;
+            if(beep){
+                updateBeepProbabilities();
+            }
+            else{
+                updateNotBeepProbabities();
+            }
+
+            List<Cell> path = pathFinder();
+            for (Cell cell : path) {
+                botPosition = cell;
+                if(botPosition==leakPosition){
+                    return actions;
+                }
+                else{
+                    updateBotProbabilities();
+                }
+                actions+=1;
+            }
+
+        }
+        return actions;
+    }
+
+    private void updateNotBeepProbabities() {
+        double totalProbability = 0.0;
+
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                if (isOpen(new Cell(i, j))) {
+                    double beepProbability = calculateBeepProbability(new Cell(i, j), botPosition);
+                    probabilityGrid[i][j] = probabilityGrid[i][j] / (1.0 - beepProbability);
+                } 
+
+                totalProbability += probabilityGrid[i][j];
+            }
+        }
+
+        // Normalize probabilities
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                probabilityGrid[i][j] /= totalProbability;
+            }
+        }
+    }
+
+    private void updateBeepProbabilities() {
+        double totalProbability = 0.0;
+
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                if (isOpen(new Cell(i, j))) {
+                    double beepProbability = calculateBeepProbability(new Cell(i, j), botPosition);
+                    probabilityGrid[i][j] = probabilityGrid[i][j] * beepProbability;
+                } 
+
+                totalProbability += probabilityGrid[i][j];
+            }
+        }
+
+        // Normalize probabilities
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                probabilityGrid[i][j] /= totalProbability;
+            }
+        }
     }
 
     private void initializeKnowledgeGrid() {
         for (int i = 0; i < shipSize; i++) {
             for (int j = 0; j < shipSize; j++) {
-                
-                knowledgeGrid[i][j] = 1/(numOpenCells);
+                knowledgeGrid[i][j] = 1.0 / numOpenCells;
             }
         }
     }
+
+    private void initializeProbabilityGrid() {
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                if (!botPosition.equals(new Cell(i, j)) && !leakPosition.equals(new Cell(i, j))) {
+                    probabilityGrid[i][j] = 1.0 / numOpenCells;
+                }
+            }
+        }
+    }
+
 
     private Cell getRandomOpenCell(List<Cell> openCells) {
         return openCells.get(random.nextInt(openCells.size()));
     }
 
-    private void initializeLeaks() {
-        List<Cell> openCells = new ArrayList<>(ship.getOpenCells());
-        openCells.remove(botPosition);
+    
+    private double calculateBeepProbability(Cell cell, Cell cell2) {
+        int d = getDistance(cell, cell2);
+        return Math.exp(-alpha * d - 1);
+    }
 
-        int numLeaks = botStrategy >= 5 && botStrategy < 10 ? 2 : 1;
-        for (int i = 0; i < numLeaks; i++) {
-            Cell leakPosition = getLeakPosition();
-            leaks.add(leakPosition);
+    private void updateBotProbabilities() {
+        double totalProbability = 0.0;
+
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                if (isOpen(new Cell(i, j))) {
+                    double beepProbability = calculateBeepProbability(new Cell(i, j), botPosition);
+                    probabilityGrid[i][j] = probabilityGrid[i][j] * (1.0 - beepProbability);
+                } 
+
+                totalProbability += probabilityGrid[i][j];
+            }
+        }
+
+        // Normalize probabilities
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                probabilityGrid[i][j] /= totalProbability;
+            }
         }
     }
 
-    private Cell getLeakPosition() {
-        // Get bot's position
-        Cell botPos = bot.getPosition();
+    private int getDistance(Cell start, Cell target) {
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // Assuming movement in 4 directions
 
-        // Determine the detection square bounds
-        int leftBound = botPos.getX() - k;
-        int rightBound = botPos.getX() + k;
-        int upperBound = botPos.getY() - k;
-        int lowerBound = botPos.getY() + k;
+        int rows = probabilityGrid.length;
+        int cols = probabilityGrid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
 
-        // Get all open cells from the ship
-        Set<Cell> allOpenCells = new HashSet<>(ship.getOpenCells());
+        Queue<Cell> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start.getX()][start.getY()] = true;
 
-        // Collect cells that are inside the detection square
-        Set<Cell> detectionSquareCells = new HashSet<>();
-        for (int x = leftBound; x <= rightBound; x++) {
-            for (int y = upperBound; y <= lowerBound; y++) {
-                Cell cell = new Cell(x, y);
-                if (allOpenCells.contains(cell)) {
-                    detectionSquareCells.add(cell);
+        int distance = 0;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                Cell current = queue.poll();
+
+                if (current.equals(target)) {
+                    return distance; // Shortest distance found.
+                }
+
+                for (int[] direction : directions) {
+                    int newRow = current.getX() + direction[0];
+                    int newCol = current.getY() + direction[1];
+
+                    if (isValid(newRow, newCol, rows, cols) && probabilityGrid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
+                        Cell nextCell = new Cell(newRow, newCol);
+                        queue.add(nextCell);
+                        visited[newRow][newCol] = true;
+                    }
+                }
+            }
+
+            distance++; // Move to the next level (increasing distance).
+        }
+
+        // If no path is found, return -1 (or any suitable value to indicate no path).
+        return -1;
+    }
+    
+    public boolean isOpen(Cell cell) {
+        // Implement logic to check if a cell is open.
+        return ship.getOpenCells().contains(cell);
+    }
+
+    private boolean sense() {
+        double beepProbability = calculateBeepProbability(botPosition,leakPosition);
+        double num = random.nextDouble();
+
+        if (num > beepProbability) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private List<Cell> pathFinder() {
+        double maximumProbability = 0.0;
+        List<Cell> bestCells = new ArrayList<>();
+
+        for (int i = 0; i < shipSize; i++) {
+            for (int j = 0; j < shipSize; j++) {
+                if (probabilityGrid[i][j] > maximumProbability) {
+                    maximumProbability = probabilityGrid[i][j];
+                    bestCells.clear();
+                    bestCells.add(new Cell(i, j));
+                } else if (probabilityGrid[i][j] == maximumProbability) {
+                    bestCells.add(new Cell(i, j));
                 }
             }
         }
 
-        // Subtract detection square cells from all open cells to get potential leak positions
-        allOpenCells.removeAll(detectionSquareCells);
+        if (bestCells.size() > 1) {
+            bestCells.sort((cell1, cell2) -> Integer.compare(getDistance(botPosition, cell1), getDistance(botPosition, cell2)));
+        }
 
-        // Randomly select a leak position
-        return getRandomOpenCell(new ArrayList<>(allOpenCells));
+        Cell chosenCell;
+
+        if (bestCells.isEmpty()) {
+            chosenCell = botPosition;
+        } else {
+            chosenCell = bestCells.get(0);
+        }
+
+        // Use BFS to move the bot to the chosen cell (implement BFS logic)
+        // Check if the bot is on the leak
+        if (chosenCell.equals(leakPosition)) {
+            putOutLeak();
+        }
+
+        return findPathToChosenCell(chosenCell, probabilityGrid);
     }
 
-    // Additional methods such as game loop, updates, etc., would be here.
+    private List<Cell> findPathToChosenCell(Cell chosenCell, double[][] grid) {
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // Assuming movement in 4 directions
+    
+        int rows = grid.length;
+        int cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        Cell[][] parent = new Cell[rows][cols];
+    
+        Queue<Cell> queue = new LinkedList<>();
+        queue.add(botPosition);
+        visited[botPosition.getX()][botPosition.getY()] = true;
+    
+        while (!queue.isEmpty()) {
+            Cell current = queue.poll();
+    
+            if (current.equals(chosenCell)) {
+                // Path found, backtrack to get the full path.
+                return getPath(botPosition, chosenCell, parent);
+            }
+    
+            for (int[] direction : directions) {
+                int newRow = current.getX() + direction[0];
+                int newCol = current.getY() + direction[1];
+    
+                if (isValid(newRow, newCol, rows, cols) && grid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
+                    Cell nextCell = new Cell(newRow, newCol);
+                    queue.add(nextCell);
+                    visited[newRow][newCol] = true;
+                    parent[newRow][newCol] = current;
+                }
+            }
+        }
+    
+        // No path found, return an empty list.
+        return new ArrayList<>();
+    }
+
+    private boolean isValid(int x, int y, int rows, int cols) {
+        return x >= 0 && x < rows && y >= 0 && y < cols;
+    }
+    
+    private List<Cell> getPath(Cell start, Cell end, Cell[][] parent) {
+        List<Cell> path = new ArrayList<>();
+        Cell current = end;
+    
+        while (!current.equals(start)) {
+            path.add(current);
+            current = parent[current.getX()][current.getY()];
+        }
+    
+        Collections.reverse(path);
+        return path;
+    }
+
+    private void putOutLeak() {
+        // Implement logic to put out the leak
+    }
+
+    // Implement additional methods such as game loop, updates, etc.
 
     // Cell class may be a part of Ship or its own file depending on the structure of your project
 }
